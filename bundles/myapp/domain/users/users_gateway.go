29ec0c0741874d48ecbc1debbecd80ae3a9bff66
yiqing <yiqing_95@qq.com>
2019-06-11 08:58:01 +0800

    玩下 PEE 企业设计模式中的目录规划

---
+++ bundles/myapp/domain/users/users_gateway.go
@@ -0,0 +1,5 @@
+package users
+
+// Gateway 封装用户表操作相关的sql语句
+type Gateway struct {
+}
--- cmd/dbstair-cli/main.go
+++ cmd/dbstair-cli/main.go
@@ -10,11 +10,49 @@ import (
 
 func main() {
 	app := cli.NewApp()
-	app.Name = "greet"
-	app.Usage = "fight the loneliness!"
-	app.Action = func(c *cli.Context) error {
-		fmt.Println("Hello friend!")
-		return nil
+
+	app.Commands = []cli.Command{
+		{
+			Name:    "add",
+			Aliases: []string{"a"},
+			Usage:   "add a task to the list",
+			Action: func(c *cli.Context) error {
+				fmt.Println("added task: ", c.Args().First())
+				return nil
+			},
+		},
+		{
+			Name:    "complete",
+			Aliases: []string{"c"},
+			Usage:   "complete a task on the list",
+			Action: func(c *cli.Context) error {
+				fmt.Println("completed task: ", c.Args().First())
+				return nil
+			},
+		},
+		{
+			Name:    "template",
+			Aliases: []string{"t"},
+			Usage:   "options for task templates",
+			Subcommands: []cli.Command{
+				{
+					Name:  "add",
+					Usage: "add a new template",
+					Action: func(c *cli.Context) error {
+						fmt.Println("new task template: ", c.Args().First())
+						return nil
+					},
+				},
+				{
+					Name:  "remove",
+					Usage: "remove an existing template",
+					Action: func(c *cli.Context) error {
+						fmt.Println("removed task template: ", c.Args().First())
+						return nil
+					},
+				},
+			},
+		},
 	}
 
 	err := app.Run(os.Args)
