121863c7d050b54d3db60122f4723d15f9e1730c
yiqing <yiqing_95@qq.com>
2019-09-30 22:43:42 +0800

    flag 相关知识

--- apps/giisample/cmd/gii-sample/main.go
+++ apps/giisample/cmd/gii-sample/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"dbstair/apps/giisample/config"
 	"dbstair/apps/giisample/server"
+	"flag"
 	"fmt"
 	"github.com/pkg/errors"
 	"os"
@@ -15,6 +16,22 @@ func main() {
 }
 
 func run() error {
+	// ## flag 配置
+	// Define a new command-line flag with the name 'addr', a default value of ":4000"
+	// and some short help text explaining what the flag controls. The value of the
+	// flag will be stored in the addr variable at runtime.
+	addr := flag.String("addr", ":4000", "HTTP network address")
+
+	// Importantly, we use the flag.Parse() function to parse the command-line flag.
+	// This reads in the command-line flag value and assigns it to the addr
+	// variable. You need to call this *before* you use the addr variable
+	// otherwise it will always contain the default value of ":4000". If any errors are
+	// encountered during parsing the application will be terminated.
+	flag.Parse()
+	_ = addr // 空消耗
+
+	// -----------------------------------------------------------------------------------------------------
+
 	//  ## 加载配置
 	// https://godoc.org/github.com/coreos/etcd/pkg/fileutil    fileutil检测文件的存在性
 	// https://github.com/etcd-io/etcd/blob/master/pkg/fileutil/fileutil.go
