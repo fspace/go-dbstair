cd67fdf9d0d3fd76c4e13ed02f5d40fa955d9ef1
yiqing <yiqing_95@qq.com>
2019-10-02 19:16:00 +0800

    最小化依赖的违反

--- apps/giisample/routes/routes.go
+++ apps/giisample/routes/routes.go
@@ -9,6 +9,7 @@ import (
 
 // InitRoutes buildRouter for mux
 func InitRoutes(r *mux.Router, app *core.Application) {
+	// FIXME 注意这里可以最做小化依赖根据迪米特法则 需要什么依赖什么 不需要的先不要引入   我们实际依赖的sql.DB 而不是Application 后期防止依赖更多东西 所以干脆一次性到位
 	// ## for userHandler
 	// 可以用curl请求测试： curl localhost:6666/user/create -X PUT|POST|DELETE -v
 	//r.HandleFunc("/user/list", func(w http.ResponseWriter, r *http.Request) {
